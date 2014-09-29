93da06e4b5adb9767f353926491b6b18da8ea828
robertsone <rufrrufr@yahoo.com>
2014-09-29 08:44:55 -0400

    dgdg

---
+++ Chapter six/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter_six
+{
+    class Program
+    {
+        public static int question(string prompt)
+        {
+            System.Console.Write(prompt);
+            int responce = Convert.ToInt32(System.Console.ReadLine());
+            return responce;
+        }
+        public static void InchesToFeet(int inches,int feet,int yards)
+        {
+            if (inches >= 12)
+            {
+                feet += inches / 12;
+                inches = inches % 12;
+            }
+            if (feet >= 3)
+            {
+                yards += feet / 3;
+                feet = feet % 3;
+            }
+            System.Console.WriteLine(inches + " Inches " + feet + " Feet " + yards + " Yards");
+        }
+        static void Main(string[] args)
+        {
+            int inches=question("How many inches:");
+            int feet = question("How many feet:");
+            int yards = question("How many yards:");
+            InchesToFeet(inches, feet, yards);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static string question(string prompt)
+        {
+            System.Console.WriteLine(prompt);
+            string responce = System.Console.ReadLine();
+            return responce;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
